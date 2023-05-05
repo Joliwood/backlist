298b83058d114e41fe88eed4006046d2a1a79c5f
Guillaume <jolibois.contact@gmail.com>
2023-05-05 15:41:17 +0200

    :hammer: structure ready in tailwindcss

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/header";
+import FirstSection from "./components/firstSection";
+import SecondSection from "./components/secondSection";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <FirstSection />
+      <SecondSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
