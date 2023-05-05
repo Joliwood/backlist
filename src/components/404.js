46fd1a067defc49436442082d1473cf68c604d3f
Guillaume JOLIBOIS <guillaumejolibois@MacBook-Air-de-Tetsara.local>
2023-05-05 20:16:31 +0200

    :construction: WIP

---
+++ src/components/404.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Error404() {
+  return <div>You have an error 404, not very lucky...</div>;
+}
+
+export default Error404;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import "./index.css";
 import "./reset.css";
 import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import express from "express";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
-);
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("the first test works =");
+});
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//       </Routes>
+//     </Router>
+//   </React.StrictMode>
+// );
+
+server.listen(3000);
