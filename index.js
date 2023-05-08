3b54baec6af49916d569d074740f7fcccbb8ec9b
Guillaume JOLIBOIS <guillaumejolibois@MacBook-Air-de-Tetsara.local>
2023-05-08 16:01:09 +0200

    :hammer: conversion from react to pure js / node / express

---
+++ index.js
@@ -0,0 +1,17 @@
+// import "./index.css";
+// import "./reset.css";
+// import App from "./src/App";
+const express = require("express");
+// const header = require("./views/header.ejs");
+
+const server = express();
+server.set("view engine", "ejs");
+server.use(express.static("public"));
+
+server.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.use((req, res, next) => res.status(404).render("404"));
+
+server.listen(3000);
